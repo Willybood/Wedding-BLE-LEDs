3cae39251e38f916d740f2e12780b178109e1dfc
Billy Wood <Willybood@yahoo.co.uk>
2023-12-09 22:35:09 +0000

    Added fixed LED keypad colours

--- bleLedRemote/main.py
+++ bleLedRemote/main.py
@@ -18,9 +18,26 @@ ble = bluetooth.BLE()
 bleTransmitter = BLERemoteTransmitter(ble, bleName, bleIntervalMs)
 keypad = RGBKeypad()
 keypad.brightness = unselectedBrightness
-keypad.color = (0, 255, 255)
 
-def runLeds():
+def setupKeyColours():
+    keypad[0, 0].color = (255, 255, 0)
+    keypad[1, 0].color = (0, 0, 255)
+    keypad[2, 0].color = (128, 0, 128)
+    keypad[3, 0].color = (255, 0, 0)
+    keypad[0, 1].color = (255, 255, 0)
+    keypad[1, 1].color = (0, 255, 255)
+    keypad[2, 1].color = (128, 0, 128)
+    keypad[3, 1].color = (255, 0, 0)
+    keypad[0, 2].color = (255, 255, 0)
+    keypad[1, 2].color = (0, 0, 255)
+    keypad[2, 2].color = (128, 0, 128)
+    keypad[3, 2].color = (255, 0, 0)
+    keypad[0, 3].color = (0, 0, 255)
+    keypad[1, 3].color = (255, 0, 0)
+    keypad[2, 3].color = (240, 255, 255)
+    keypad[3, 3].color = (0, 255, 0)
+
+def processKeys():
     previousKey = [defaultButtons[0], defaultButtons[1]]
     currentlySelectedKey = [defaultButtons[0], defaultButtons[1]]
     keypad[defaultButtons[0], defaultButtons[1]].brightness = 1
@@ -52,4 +69,5 @@ def runLeds():
                     
 if __name__ == "__main__":
     bleTransmitter.advertise(defaultButtons[0], defaultButtons[1])
-    runLeds()
+    setupKeyColours()
+    processKeys()
